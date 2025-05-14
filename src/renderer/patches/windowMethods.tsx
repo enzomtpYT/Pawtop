7e691b289050726dfeaff4dd9df220c1fc129b93
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-05-14 13:54:57 -0400

    Fix Windows Methods

--- src/renderer/patches/windowMethods.tsx
+++ src/renderer/patches/windowMethods.tsx
@@ -14,12 +14,12 @@ addPatch({
                 {
                     // eslint-disable-next-line no-useless-escape
                     match: /\i\.window\.(close|minimize|maximize)/g,
-                    replace: `VesktopNative.win.$1`
+                    replace: "VesktopNative.win.$1"
                 },
                 {
                     // TODO: Fix eslint rule
                     // eslint-disable-next-line no-useless-escape
-                    match: /(focus(\(\i\))){.{0,150}?\.focus\(\i,\i\)/,
+                    match: /(focus(\(\i\)){).{0,150}?\.focus\(\i,\i\)/,
                     replace: "$1VesktopNative.win.focus$2"
                 }
             ]
