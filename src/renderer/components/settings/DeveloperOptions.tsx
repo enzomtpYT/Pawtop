01dbe3346618d5bb6f7e9ec11a1ec012d54a85cb
enzomtp <enzojore@live.fr>
2026-08-30 21:30:33 +0200

    fix: fix the cr empty spaces after lines in header of 5 files

--- src/renderer/components/settings/DeveloperOptions.tsx
+++ src/renderer/components/settings/DeveloperOptions.tsx
@@ -1,11 +1,7 @@
 /*
-
  * Vesktop, a desktop app aiming to give you a snappier Discord Experience
-
  * Copyright (c) 2026 Vendicated and Vesktop contributors
-
  * SPDX-License-Identifier: GPL-3.0-or-later
-
  */
 
 import { Button, Heading, Paragraph, TextButton } from "@equicord/types/components";
